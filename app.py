ba7d0102f15e8944b52a361396f54466a347bd7d
Chaim Finkelman <chaim@Chaims-MBP.localdomain>
2018-08-08 12:16:23 -0700

    add json and rc lookup route

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
+import json
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-  return "Hello World!"
+def landing():
+  return json.dumps("This is an unofficial bard catalog.")
+
+@app.route('/<rc_number>')
+def rc_number(rc_number):
+  return json.dumps({"rc_number": rc_number})
